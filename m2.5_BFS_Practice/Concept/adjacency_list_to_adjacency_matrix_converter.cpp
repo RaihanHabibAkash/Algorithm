aa072c03c136d3b63cf24f23f360fb4c19379e7b
RaihanHabibAkash <raihanakash2003811@gmail.com>
2026-07-16 14:22:59 +0600

    Adjacency list to Adjacency Matrix converter

---
+++ m2.5_BFS_Practice/Concept/adjacency_list_to_adjacency_matrix_converter.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> adj_list[100];
+
+void adjacency_list_to_Adjacency_matrix_converter(int adj_mat[][100], int nodes) {
+    for(int i = 0; i < nodes; i++) {
+        for(int j = 0; j < adj_list[i].size(); j++) {
+            adj_mat[i][adj_list[i][j]] = 1;
+            adj_mat[adj_list[i][j]][i] = 1;
+        }
+    }
+
+}
+
+int main() {
+    int nodes, edges; cin >> nodes >> edges;
+    while(edges--) {
+        int a, b; cin >> a >> b;
+        adj_list[a].push_back(b);
+        adj_list[b].push_back(a);
+    }
+
+    int adj_mat[100][100];
+    memset(adj_mat, 0, sizeof(adj_mat));
+
+    adjacency_list_to_Adjacency_matrix_converter(adj_mat, nodes);
+
+    // print
+    for(int i = 0; i < nodes; i++) {
+        for(int j = 0; j < nodes; j++)
+            cout << adj_mat[i][j] << " ";
+        cout << endl;
+    }
+
+    return 0;
+}
