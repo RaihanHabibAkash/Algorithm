00924198cc9236ff2cbd2f8e89a4f2d211263771
RaihanHabibAkash <raihanakash2003811@gmail.com>
2026-07-16 12:39:48 +0600

    Adjacency matrix to adjacency list convertation

--- m2.5_BFS_Practice/Concept/adjacency_matrix_to_adjacency_list.cpp
+++ m2.5_BFS_Practice/Concept/adjacency_matrix_to_adjacency_list.cpp
@@ -1,31 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void adj_mat_to_adj_list_converter(int adj_mat[][100], int n) {
-    vector<int> adj_list[n];
+vector<int> adj_list[100];
 
+void adj_mat_to_adj_list_converter(int adj_mat[][100], int n) {
     for(int i = 0; i < n; i++)
         for(int j = 0; j < n; j++)
-            if(adj_mat[i][j] == 1) adj_list[i].push_back(j);
-            
-    for(int i = 0; i < n; i++) {
-        cout << i << " -> ";
-        for(int j = 0; j < adj_list[i].size(); j++) // we can use range base for loop
-            cout << adj_list[i][j] << " ";
-        cout << endl;
-    }
+            if(adj_mat[i][j] == 1)
+                adj_list[i].push_back(j);
 }
 
 int main() {
-    int n; cin >> n;
     int adj_mat[100][100];
-    memset(adj_mat, 0, sizeof(adj_mat));
+    int n; cin >> n;
 
     for(int i = 0; i < n; i++)
         for(int j = 0; j < n; j++)
             cin >> adj_mat[i][j];
-
+    
     adj_mat_to_adj_list_converter(adj_mat, n);
+
+    for(int i = 0; i < n; i++) {
+        cout << i << " -> ";
+        for(int j = 0; j < adj_list[i].size(); j++)
+            cout << adj_list[i][j] << " ";
+        cout << endl;
+    }
 
     return 0;
 }
