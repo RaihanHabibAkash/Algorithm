eb4e906adbc0e4ba87f88c5f045ace58bb986c85
RaihanHabibAkash <raihanakash2003811@gmail.com>
2026-07-16 14:44:46 +0600

    Added Range base for loop as well for understanding

--- m2.5_BFS_Practice/Concept/adjacency_list_to_adjacency_matrix_converter.cpp
+++ m2.5_BFS_Practice/Concept/adjacency_list_to_adjacency_matrix_converter.cpp
@@ -5,10 +5,19 @@ vector<int> adj_list[100];
 
 void adjacency_list_to_Adjacency_matrix_converter(int adj_mat[][100], int nodes) {
     for(int i = 0; i < nodes; i++) {
-        for(int j = 0; j < adj_list[i].size(); j++) { // can use range base for loop
+        for(int j = 0; j < adj_list[i].size(); j++) { 
             adj_mat[i][adj_list[i][j]] = 1;
             adj_mat[adj_list[i][j]][i] = 1;
         }
+        
+        /* can use range base for loop
+        for(int i = 0; i < nodes; i++) {
+            for(int val : adj_list[i]) {
+                adj_mat[i][val] = 1;
+                adj_mat[val][i] = 1;
+            }
+        }
+        */
     }
 
 }
