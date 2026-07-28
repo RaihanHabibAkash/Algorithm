9637dc7ae852e3dc63c1b53ba67d13cee2f1ac5e
RaihanHabibAkash <raihanakash2003811@gmail.com>
2026-07-28 11:48:01 +0600

    LeetCode Practice

--- m5_Graph_LeetCode/pra.cpp
+++ m5_Graph_LeetCode/pra.cpp
@@ -1,36 +1,52 @@
 class Solution {
 public:
-    bool vis[200005];
-    vector<int> adj_list[200005];
+    int rows, cols, cnt;
+    bool vis[55][55];
+    vector<pair<int,int>> mv = {{-1,0},{1,0},{0,-1},{0,1}};
 
-    void bfs(int src) {
-        queue<int> q;
-        q.push(src);
-        vis[src] = true;
-
-        while(!q.empty()) {
-            int par = q.front();
-            q.pop();
-
-            for(int child : adj_list[par]) {
-                if(!vis[child]) {
-                    q.push(child);
-                    vis[child] = true;
-                }
-            }
-        }
+    bool valid(int i, int j) {
+        if(i < 0 || i >= rows || j < 0 || j >= cols)
+            return false;
+        return true;
     }
 
-    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+    void bfs(vector<vector<int>>& grid, int i, int j) {
+        queue<pair<int,int>> q;
+        q.push({i, j});
+        vis[i][j] = true;
+
+        while(!q.empty()) {
+            int si = q.front().first,
+                sj = q.front().second;
+
+            q.pop();
+
+            cnt++;
+
+            for(int ind = 0; ind < 4; ind++) {
+                int ci = si + mv[ind].first,
+                    cj = sj + mv[ind].second;
+                if(valid(ci, cj) && grid[ci][cj] == 1 && !vis[ci][cj]) {
+                    q.push({ci, cj});
+                    vis[ci][cj] = true;
+                }
+            }
+        } 
+    }
+
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        rows = grid.size();
+        cols = grid[0].size();
         memset(vis, false, sizeof(vis));
+        int mx = 0;
 
-        for(vector<int> x : edges) {
-            adj_list[x[0]].push_back(x[1]);
-            adj_list[x[1]].push_back(x[0]);
-        }
-
-        bfs(source);
-
-        return vis[destination];
+        for(int i = 0; i < rows; i++)
+            for(int j = 0; j < cols; j++)
+                if(grid[i][j] == 1 && !vis[i][j]) {
+                    cnt = 0;
+                    bfs(grid, i, j);
+                    if(mx < cnt) mx = cnt;
+                }
+        return mx;
     }
 };
