895ceef8e3c1e68f51736c1129456f6efea0061e
RaihanHabibAkash <raihanakash2003811@gmail.com>
2026-07-28 11:56:04 +0600

    LeetCode Practice

--- m5_Graph_LeetCode/pra.cpp
+++ m5_Graph_LeetCode/pra.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
-    int rows, cols, cnt;
-    bool vis[55][55];
+    int rows, cols;
     vector<pair<int,int>> mv = {{-1,0},{1,0},{0,-1},{0,1}};
+    bool vis[305][305];
 
     bool valid(int i, int j) {
         if(i < 0 || i >= rows || j < 0 || j >= cols)
@@ -10,43 +10,40 @@ public:
         return true;
     }
 
-    void bfs(vector<vector<int>>& grid, int i, int j) {
+    void bfs(vector<vector<char>>& grid, int si, int sj) {
         queue<pair<int,int>> q;
-        q.push({i, j});
-        vis[i][j] = true;
+        q.push({si, sj});
+        vis[si][sj] = true;
 
         while(!q.empty()) {
-            int si = q.front().first,
-                sj = q.front().second;
-
+            int pi = q.front().first,
+                pj = q.front().second;
             q.pop();
 
-            cnt++;
+            for(int i = 0; i < 4; i++) {
+                int ci = pi + mv[i].first,
+                    cj = pj + mv[i].second;
 
-            for(int ind = 0; ind < 4; ind++) {
-                int ci = si + mv[ind].first,
-                    cj = sj + mv[ind].second;
-                if(valid(ci, cj) && grid[ci][cj] == 1 && !vis[ci][cj]) {
+                if(valid(ci, cj) && !vis[ci][cj] && grid[ci][cj] == '1') {
                     q.push({ci, cj});
                     vis[ci][cj] = true;
                 }
             }
-        } 
+        }
     }
 
-    int maxAreaOfIsland(vector<vector<int>>& grid) {
+    int numIslands(vector<vector<char>>& grid) {
         rows = grid.size();
         cols = grid[0].size();
+        int cnt = 0;
         memset(vis, false, sizeof(vis));
-        int mx = 0;
 
         for(int i = 0; i < rows; i++)
             for(int j = 0; j < cols; j++)
-                if(grid[i][j] == 1 && !vis[i][j]) {
-                    cnt = 0;
+                if(grid[i][j] == '1' && !vis[i][j]) {
                     bfs(grid, i, j);
-                    if(mx < cnt) mx = cnt;
+                    cnt++;
                 }
-        return mx;
+        return cnt;
     }
 };
