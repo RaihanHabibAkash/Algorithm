1be18ffe28c184aebceba302fdebce969427ecc9
RaihanHabibAkash <raihanakash2003811@gmail.com>
2026-07-28 12:16:39 +0600

    Practice

--- m5_Graph_LeetCode/pra.cpp
+++ m5_Graph_LeetCode/pra.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
+    bool vis[505][505];
+    vector<pair<int,int>> mv = {{-1,0},{1,0},{0,-1},{0,1}};
     int rows, cols;
-    vector<pair<int,int>> mv = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-    bool vis[105][105];
-
+    bool flag;
+    
     bool valid(int i, int j) {
         if(i < 0 || i >= rows || j < 0 || j >= cols)
             return false;
         return true;
     }
 
-    void bfs(vector<vector<int>>& grid, int si, int sj) {
+    void bfs(vector<vector<int>>& grid1, vector<vector<int>>& grid2, int si, int sj) {
         queue<pair<int,int>> q;
         q.push({si, sj});
         vis[si][sj] = true;
@@ -20,10 +21,12 @@ public:
                 pj = q.front().second;
             q.pop();
 
+            if(grid1[pi][pj] != 1) flag = false;
+
             for(int i = 0; i < 4; i++) {
                 int ci = pi + mv[i].first,
                     cj = pj + mv[i].second;
-                if(valid(ci, cj) && grid[ci][cj] == 0 && !vis[ci][cj]) {
+                if(valid(ci, cj) && grid2[ci][cj] == 1 && !vis[ci][cj]) {
                     q.push({ci, cj});
                     vis[ci][cj] = true;
                 }
@@ -31,23 +34,18 @@ public:
         }
     }
 
-    int closedIsland(vector<vector<int>>& grid) {
-        rows = grid.size();
-        cols = grid[0].size();
-
+    int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
+        rows = grid2.size();
+        cols = grid2[0].size();
         memset(vis, false, sizeof(vis));
-
         int cnt = 0;
-        for(int i = 0; i < rows; i++)
-            for(int j = 0; j < cols; j++)
-                if((i == 0 || j == 0 || i == rows-1 || j == cols-1) && grid[i][j] == 0 && !vis[i][j])
-                    bfs(grid, i, j);
 
-        for(int i = 0; i < rows; i++)
+        for(int i = 0; i < rows; i++) 
             for(int j = 0; j < cols; j++)
-                if(grid[i][j] == 0 && !vis[i][j]) {
-                    bfs(grid, i, j);
-                    cnt++;
+                if(grid2[i][j] == 1 && !vis[i][j]) {
+                    flag = true;
+                    bfs(grid1, grid2, i, j);
+                    if(flag) cnt++;
                 }
         return cnt;
     }
