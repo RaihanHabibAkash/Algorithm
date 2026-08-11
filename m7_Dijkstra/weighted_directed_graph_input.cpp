a7c9115509cf119a7905c2c757b4276b9f03fa0c
RaihanHabibAkash <raihanakash2003811@gmail.com>
2026-08-11 18:37:39 +0600

    Weighted Graph input for Adjacency list

---
+++ m7_Dijkstra/weighted_directed_graph_input.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int nodes, edges; cin >> nodes >> edges;
+    vector<pair<int,int>> adj_list[nodes];
+
+    // Taking input
+    while(edges--) {
+        int a,b,c; cin >> a >> b >> c;
+        adj_list[a].push_back({b,c});
+    }
+
+    // Printing
+    for(int i = 0; i < nodes; i++) {
+        cout << i << " -> ";
+        for(pair<int,int> p : adj_list[i])
+            cout << p.first << " " << p.second << ", ";
+        cout << endl;
+    }
+
+    return 0;
+}
