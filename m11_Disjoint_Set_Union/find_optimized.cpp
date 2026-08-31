a760b841947a5355534bc3af37ca9dffb95082bc
RaihanHabibAkash <raihanakash2003811@gmail.com>
2026-08-31 18:21:52 +0600

    DSU find optimized versioin using Recurtion & Backtracking

---
+++ m11_Disjoint_Set_Union/find_optimized.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int leader[1005];
+
+int find(int node) { // O(LogN)
+    if(leader[node] == -1)
+        return node;
+
+    int l = find(leader[node]);
+    leader[node] = l; // backtracking
+
+    return l;
+}
+
+int main() {
+    memset(leader, -1, sizeof(leader));
+
+    // Don't need to set mannualy again
+    leader[0] = 1;
+    leader[1] = -1;
+    leader[2] = 1;
+    leader[3] = 1;
+    leader[4] = 5;
+    leader[5] = 3;
+    // 0,2--> 1 <--3 <--5 <--4
+
+    cout << find(4) << endl;
+
+    return 0;
+}
