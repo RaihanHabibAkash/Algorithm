0ba6cdc97b01cc339ebb9a4698120e8ce73a572f
RaihanHabibAkash <raihanakash2003811@gmail.com>
2026-09-08 05:46:58 +0600

    Dynamic programming memorization in fibonacci

---
+++ m14_Dynamic_Programming/dp_fibonacci.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+ll dp[1005];
+
+ll fibo(int n) { // O(N)
+    if(n < 2)
+        return n; // always return 0 or 1
+
+    if(dp[n] != -1)
+        return dp[n];
+
+    return dp[n] = fibo(n-1) + fibo(n-2);
+}
+
+int main() {
+    int n; cin >> n;
+    memset(dp, -1, sizeof(dp));
+    cout << fibo(n) << endl;
+
+    return 0;
+}
