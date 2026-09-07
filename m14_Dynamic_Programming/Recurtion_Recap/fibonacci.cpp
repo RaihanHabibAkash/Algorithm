317c1d0c69745619ac58bf50db9b4fcb17bde944
RaihanHabibAkash <raihanakash2003811@gmail.com>
2026-09-08 05:46:26 +0600

    Time complexity added using comment

--- m14_Dynamic_Programming/Recurtion_Recap/fibonacci.cpp
+++ m14_Dynamic_Programming/Recurtion_Recap/fibonacci.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int fibo(int n) {
+int fibo(int n) { // O(2^N)
     if(n < 2)
         return n; // return 0 or 1 only
     return fibo(n-1) + fibo(n-2);
