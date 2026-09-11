5e802dbb1b798e7136703688a01ffe87c2f80d06
RaihanHabibAkash <raihanakash2003811@gmail.com>
2026-09-12 04:58:35 +0600

    SubSet Sum variation of 0-1 KnapSack

---
+++ m17_subSet_sum/subSet_sum.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int val[1005];
+
+bool subSet_sum(int i, int sum) {
+    if(i < 0) {
+        if(sum == 0)
+            return true;
+        else
+            return false;
+    }
+
+    bool option1 = false;
+    if(val[i] <= sum)
+        option1 = subSet_sum(i-1, sum - val[i]);
+    
+    return option1 || subSet_sum(i-1, sum);
+}
+
+int main() {
+    int n, sum; cin >> n >> sum;
+    for(int i = 0; i < n; i++)
+        cin >> val[i];
+
+    cout << (subSet_sum(n-1, sum) ? "YES" : "NO") << endl;
+
+    return 0;
+}
